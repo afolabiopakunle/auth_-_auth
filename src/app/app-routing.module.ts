690e6c17af0c865ba19709a8fe9f8baa3496f749
afolabiopakunle <opakunleafolabi@gmail.com>
2023-08-01 16:19:22 +0100

    added firebase

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'accounts', component: AccountsComponent},
+  
+]
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
+    AppRoutingModule,
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
